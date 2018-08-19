13bfc9add3a5dbd62a96bdf57137d27e0d2d4b0b
menglihuan <menglihuan7@gmail.com>
2018-08-19 11:15:34 +0800

    爬去网易歌单

--- GakkiSimle/WeiBoSpider_pyspider.py
+++ GakkiSimle/WeiBoSpider_pyspider.py
@@ -9,18 +9,19 @@ class Handler(BaseHandler):
 
     logging.basicConfig(level = logging.INFO)
 
-    client = KafkaClient(hosts = "192.168.0.101:9092",zookeeper_hosts="192.168.0.101:2181")
+    #client = KafkaClient(hosts = "192.168.0.101:9092",zookeeper_hosts="192.168.0.101:2181")
 
     #生产kafka数据，通过字符串形式
-    def produce_kafka_data(kafka_topic,result):
-        with kafka_topic.get_producer(delivery_reports=True) as producer:
-             type(json.dump(result))
-             producer.produce(json.dump(result))
+    #def produce_kafka_data(kafka_topic,result):
+        #with kafka_topic.get_producer(delivery_reports=True) as producer:
+             #type(json.dump(result))
+             #producer.produce(json.dump(result))
 
     def on_result(self, result):
         topic = self.client.topics["gakki"]
         print(result)
-        self.produce_kafka_data(topic,result)
+       # self.produce_kafka_data(topic,result)
+        #存入mongoDb
 
     #@every(minutes=24 * 60)
     def on_start(self):
@@ -31,3 +32,4 @@ class Handler(BaseHandler):
     def json_parser(self, response):
         return [{ "card": x
                   } for x in response.json['data']['cards']]
+
