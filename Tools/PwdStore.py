5c7b178b80178cc00a704729442e5bf223fcc0b7
menglihuan <menglihuan7@gmail.com>
2018-09-07 23:07:42 +0800

    修改命令行

--- Tools/PwdStore.py
+++ Tools/PwdStore.py
@@ -5,6 +5,8 @@
 """
 import argparse
 from prompt_toolkit import prompt
+import shlex
+from prompt_toolkit.contrib.completers import WordCompleter
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -12,19 +14,17 @@ from mysql.connector import errorcode
 class MyDataSource:
     config = {
         'host': '127.0.0.1',
-        'user': 'root',
-        'password': '297234',
         'port': 3306,
         'database': 'pwdstore',
-        'charset': 'utf8'
+        'charset': 'utf8',
+        'use_unicode':True
     }
 
     def __init__(self) :
         #数据库初始化
-        self.conn=None
+        self._conn=None
         try:
-            self.conn =mysql.connector.connect(host='130.24.38.26',user='root',password='297234'
-                                          ,database= 'pwdstore',use_unicode=True,charset= "utf8")
+            self._conn =mysql.connector.connect(**self.config)
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Something is wrong with your user name or password")
@@ -33,8 +33,8 @@ class MyDataSource:
             else:
                 print(err)
     @property
-    def connection(self):
-        return self.conn
+    def conn(self):
+        return self._conn
 #插入一条数据
 def store(db,name,account,password):
 
@@ -45,20 +45,35 @@ def store(db,name,account,password):
     if not password:
         raise '密码不能为空'
 
-    cursor = db.connection().cursor()
+    cursor = db.conn.cursor()
     cursor.execute('insert into account (name,account,password) values (%s, %s, %s)'
-               , [args.name, args.account, args.password])
+               , [name, account, password])
     print(f'插入行数{cursor.rowcount}')
-    db.connection().commit()
+    db.conn.commit()
     cursor.close()
 #查询全部
 def getAll(db):
-    cursor = db.connection().cursor()
+    cursor = db.conn.cursor()
     cursor.execute('select * from account')
     rows=cursor.fetchall()
     print(f'所有账号为{rows}')
     cursor.close()
-
+#初始化命令提示
+def init_command():
+    ShellCompleter = WordCompleter(['-a','-n','-p'],ignore_case=True)
+    return ShellCompleter
+#获取命令
+def tokenize(cmd):
+    cmd_list=shlex.split(cmd)
+    cmd_map={}
+    cmd_length=len(cmd_list)
+    start=0
+    while start<cmd_length:
+        key=cmd_list[start]
+        value=cmd_list[start+1]
+        cmd_map[key]=value
+        start=start+2
+    return  cmd_map
 
 
 if __name__ == '__main__':
@@ -72,25 +87,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    #db=MyDataSource()
-    #一直监听一个输入
+    db=MyDataSource()
+    #监听输入
     while True:
-        user_input = prompt('>')
-        print(user_input)
-        print(args)
-        #sorted(db,args.name,args.account,args.password)
+        cmd = prompt('>', completer=init_command())
+        if 'exit'==cmd:
+            break
+        cmd_tokens = tokenize(cmd)
+        name=cmd_tokens['-n']
+        account=cmd_tokens['-a']
+        password=cmd_tokens['-p']
+        store(db,name,account,password)
+        getAll(db)
+    db.conn.close()
 
-    # cursor = conn.cursor()
-    # cursor.execute('insert into account (name,account,password) values (%s, %s, %s)'
-    #            , [args.name, args.account, args.password])
-    # print(f'插入行数{cursor.rowcount}')
-    # conn.commit()
-    # cursor.close()
-
-    # cursor = conn.cursor()
-    # cursor.execute('select * from account')
-    # rows=cursor.fetchall()
-    # print(f'所有账号为{rows}')
-    # cursor.close()
-
-    #conn.close()
