57407a172eccd9431aa660bbb4307a0e23de9d05
menglihuan <menglihuan7@gmail.com>
2020-03-16 23:34:44 +0800

    优化代码

--- Tools/NanRenVip.py
+++ Tools/NanRenVip.py
@@ -8,34 +8,9 @@ from queue import Queue
 
 import requests
 import csv
+
+import time
 from bs4 import BeautifulSoup
-
-
-def new_task(i, headers, factory, q):
-    x = 1
-    while x < 100:
-        factory_name = factory['href']
-        print(f'片商{factory_name} 页数:{x}')
-        num = x
-        if x == 1:
-            num = ""
-        factory_url = host + factory_name + f'index{num}.html'
-        resp2 = session.get(url, headers=headers)
-        if resp2.status_code == 404:
-            continue
-        soup2 = BeautifulSoup(resp.text, "html.parser")
-        for j, content in enumerate(soup2.find_all(class_="book-layout")):
-            # if j > 1:
-            #     break
-            content_url = host + content['href']
-            img = content.img
-            video_num = img['alt']
-            video_img = img['data-echo']
-            # content_string = content.div.p.string
-            row = [i, factory_name, factory_url, content_url, video_num, video_img]
-            q.put(row)
-        x = x + 1
-
 
 session = requests.session()
 session.keep_alive = True
@@ -49,21 +24,55 @@ headers = {
     "Connection": "keep-alive"
 }
 
-resp = session.get(url, headers=headers)
-soup = BeautifulSoup(resp.text, "html.parser")
-q = Queue()
-Thread_list = []
-file = []
-for i, factory in enumerate(soup.find_all(class_="module-field-cell")):
-    t = threading.Thread(target=new_task, args=(i, headers, factory, q))
-    t.start()
-    Thread_list.append(t)
-for i in Thread_list:
-    i.join()
-while not q.empty():
-    row = q.get()
-    file.append(row)
 
-with open("nanrenvip.csv", 'a', errors='ignore', newline='') as f:
-    f_csv = csv.writer(f)
-    f_csv.writerows(file)
+def new_task(i, factory, q):
+    x = 1
+    while x < 100:
+        factory_name = factory['href']
+        print(f'片商{factory_name} 页数:{x}')
+        num = x
+        if x == 1:
+            num = ""
+        factory_url = host + factory_name + f'index{num}.html'
+        resp2 = session.get(url, headers=headers)
+        if resp2.status_code == 404:
+            continue
+        soup2 = BeautifulSoup(resp2.text, "html.parser")
+        for j, content in enumerate(soup2.find_all(class_="book-layout")):
+            # if j > 1:
+            #     break
+            content_url = host + content['href']
+            img = content.img
+            video_num = img['alt']
+            video_img = img['data-echo']
+            # content_string = content.div.p.string
+            row = [i, factory_name, factory_url, content_url, video_num, video_img]
+            q.put(row)
+        x = x + 1
+
+
+def main():
+    resp = session.get(url, headers=headers)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    q = Queue()
+    thread_list = []
+    file = []
+    for i, factory in enumerate(soup.find_all(class_="module-field-cell")):
+        t = threading.Thread(target=new_task, args=(i,  factory, q))
+        t.start()
+        thread_list.append(t)
+    for i in thread_list:
+        i.join()
+    while not q.empty():
+        row = q.get()
+        file.append(row)
+
+    with open("nanrenvip.csv", 'a', errors='ignore', newline='') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerows(file)
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    print(f'时间{time.time()-start}')
